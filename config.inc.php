7a873017fd843b968dd9b038252f0e975c77ac77
sufrtech <jangodbmail@gmail.com>
2011-10-24 01:27:03 -0400

    Config File

---
+++ config.inc.php
@@ -0,0 +1,9 @@
+<?php
+
+# Edit these settings #
+ $A_debug=0;  //dont touch
+ $A_title = "File Upload"; //Page Title
+ $A_maxsize = 2048000; //204800 = 2,048KB or 2MB
+ $A_sitename = "Site Name"//Site name
+ 
+?>
